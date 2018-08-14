83db30058aa3d6d4dcf3ae6277086aa1d6f0f855
Daniel Gibbs <me@danielgibbs.co.uk>
2018-08-14 23:58:33 +0100

    test

--- push.sh
+++ push.sh
@@ -7,11 +7,13 @@ setup_git() {
 
 commit_website_files() {
   git checkout -b ${TRAVIS_BRANCH}
-  git add . *.txt
+  git add . steamcmdcommands.txt
+  git add . lastchecked.txt
   git commit --message "Travis build: $TRAVIS_BUILD_NUMBER"
 }
 
 upload_files() {
+  echo "${GH_TOKEN}"
   git remote add origin https://${GH_TOKEN}@github.com/dgibbs64/SteamCMD-Commands-List.git > /dev/null 2>&1
   git push --set-upstream origin ${TRAVIS_BRANCH}
 }
