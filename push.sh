e27cd0b7c10976406b610096d704fe421033dc15
Daniel Gibbs <me@danielgibbs.co.uk>
2018-08-14 23:39:32 +0100

    test

--- push.sh
+++ push.sh
@@ -13,7 +13,7 @@ commit_website_files() {
 
 upload_files() {
   git remote add origin https://${GH_TOKEN}@github.com/dgibbs64/SteamCMD-Commands-List.git > /dev/null 2>&1
-  git push --set-upstream origin master
+  git push --set-upstream origin ${TRAVIS_BRANCH}
 }
 
 setup_git
