c81ee2fa494e515a95c5f31c4e43c21b21b3be88
Daniel Gibbs <me@danielgibbs.co.uk>
2018-08-15 23:18:11 +0100

    test

--- push.sh
+++ push.sh
@@ -5,10 +5,12 @@ cat "lastchecked.txt"
 git config --global user.email "me@danielgibbs.co.uk"
 git config --global user.name "dgibbs64"
 
-git checkout -b ${TRAVIS_BRANCH}
+git remote set-url origin https://dgibbs64:${GH_TOKEN}@github.com/dgibbs64/SteamCMD-Commands-List.git
+
+git checkout ${TRAVIS_BRANCH}
 git add . steamcmdcommands.txt
 git add . lastchecked.txt
 git commit --message "Travis build: $TRAVIS_BUILD_NUMBER"
 
-git remote set-url origin https://dgibbs64:${GH_TOKEN}@github.com/dgibbs64/SteamCMD-Commands-List.git
+
 git push --set-upstream origin ${TRAVIS_BRANCH}
