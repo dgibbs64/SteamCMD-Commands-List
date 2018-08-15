aff55ae94fc2a18875c51379a3a42fc3b66e3056
Daniel Gibbs <me@danielgibbs.co.uk>
2018-08-15 23:00:26 +0100

    added lastchecked.txt

--- push.sh
+++ push.sh
@@ -1,22 +1,13 @@
 #!/bin/sh
 
-setup_git() {
-  git config --global user.email "me@danielgibbs.co.uk"
-  git config --global user.name "dgibbs64"
-}
+date > lastchecked.txt
+git config --global user.email "me@danielgibbs.co.uk"
+git config --global user.name "dgibbs64"
 
-commit_website_files() {
-  git checkout -b ${TRAVIS_BRANCH}
-  git add . steamcmdcommands.txt
-  git add . lastchecked.txt
-  git commit --message "Travis build: $TRAVIS_BUILD_NUMBER"
-}
+git checkout -b ${TRAVIS_BRANCH}
+git add . steamcmdcommands.txt
+git add . lastchecked.txt
+git commit --message "Travis build: $TRAVIS_BUILD_NUMBER"
 
-upload_files() {
-  git remote set-url origin https://dgibbs64:${GH_TOKEN}@github.com/dgibbs64/SteamCMD-Commands-List.git
-  git push --set-upstream origin ${TRAVIS_BRANCH}
-}
-
-setup_git
-commit_website_files
-upload_files
+git remote set-url origin https://dgibbs64:${GH_TOKEN}@github.com/dgibbs64/SteamCMD-Commands-List.git
+git push --set-upstream origin ${TRAVIS_BRANCH}
