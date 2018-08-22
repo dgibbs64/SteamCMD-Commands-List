d615a3b3e16db16f07156c1b28d193a2c3cbe32a
Daniel Gibbs <me@danielgibbs.co.uk>
2018-08-22 22:56:25 +0100

    removed lastchecked.txt
    travis will show when the last build was completed

--- push.sh
+++ push.sh
@@ -1,7 +1,5 @@
 #!/bin/sh
 
-date > lastchecked.txt
-cat "lastchecked.txt"
 git config --global user.email "me@danielgibbs.co.uk"
 git config --global user.name "dgibbs64"
 
@@ -9,7 +7,6 @@ git remote set-url origin https://dgibbs64:${GH_TOKEN}@github.com/dgibbs64/Steam
 
 git checkout ${TRAVIS_BRANCH}
 git add . steamcmdcommands.txt
-git add . lastchecked.txt
 git commit --message "Travis build: $TRAVIS_BUILD_NUMBER"
 
 
