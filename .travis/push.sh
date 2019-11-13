e7d26e115091dfa4ce282e671dfbbabf74ce3a11
Daniel Gibbs <me@danielgibbs.co.uk>
2019-11-13 10:43:00 +0000

    Update push.sh

--- .travis/push.sh
+++ .travis/push.sh
@@ -5,7 +5,7 @@ git config --global user.name "dgibbs64"
 
 git remote set-url origin https://dgibbs64:${GH_TOKEN}@github.com/dgibbs64/SteamCMD-Commands-List.git
 
-git checkout ${TRAVIS_BRANCH}
+git checkout "${TRAVIS_BRANCH}"
 git add . steamcmd_commands.txt
 git commit --message "Travis build: $(date +%Y-%m-%d)"
-git push --set-upstream origin ${TRAVIS_BRANCH}
+git push --set-upstream origin "${TRAVIS_BRANCH}"
