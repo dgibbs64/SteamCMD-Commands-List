d3711cb480b985b931d6f15a16091546a3a73a30
Daniel Gibbs <me@danielgibbs.co.uk>
2022-08-18 17:16:50 +0100

    updated

--- steamcmd_commands.sh
+++ steamcmd_commands.sh
@@ -9,55 +9,37 @@
 
 rootdir="$(dirname "$(readlink -f "${BASH_SOURCE[0]}")")"
 
-# Install SteamCMD
-echo ""
-echo "Installing SteamCMD"
-echo "================================="
-cd "${rootdir}" || exit
-mkdir -pv "steamcmd"
-cd "steamcmd" || exit
-if [ ! -f "steamcmd.sh" ]; then
-    echo -e "downloading steamcmd_linux.tar.gz...\c"
-    wget -N /dev/null http://media.steampowered.com/client/steamcmd_linux.tar.gz 2>&1 | grep -F HTTP | cut -c45-| uniq
-    tar --verbose -zxf "steamcmd_linux.tar.gz"
-    rm -v "steamcmd_linux.tar.gz"
-    chmod +x "steamcmd.sh"
-else
-    echo "Steam already installed!"
-fi
-
 echo ""
 echo "Getting SteamCMD Commands/Convars"
 echo "================================="
 mkdir "${rootdir}/tmp"
 cd "${rootdir}/steamcmd" || exit
 # Loop though each letter of the alphabet using find command
-for letter in {a..z}
-do
-    echo "./steamcmd.sh +login anonymous +find ${letter} +quit"
-    ./steamcmd.sh +login anonymous +find ${letter} +quit > "${rootdir}/tmp/${letter}_raw.txt"
-    # Remove ANSI characters
-    sed -i 's/\x1b//g' "${rootdir}/tmp/${letter}_raw.txt"
-    sed -i 's/\[0m//g' "${rootdir}/tmp/${letter}_raw.txt"
-    sed -i 's/\[1m//g' "${rootdir}/tmp/${letter}_raw.txt"
-    # Remove CWorkThreadPool errors
-    sed -i '/CWorkThreadPool/d' "${rootdir}/tmp/${letter}_raw.txt"
-    sed -i '/workthreadpool.cpp/d' "${rootdir}/tmp/${letter}_raw.txt"
-    sed -i '/CProcessWorkItem/d' "${rootdir}/tmp/${letter}_raw.txt"
-    sed -i '/CHTTPClientThreadPool/d' "${rootdir}/tmp/${letter}_raw.txt"
-    sed -i '/CJobMgr::m_WorkThreadPool:1/d' "${rootdir}/tmp/${letter}_raw.txt"
+for letter in {a..z}; do
+  echo "steamcmd.sh +login anonymous +find ${letter} +quit"
+  steamcmd.sh +login anonymous +find ${letter} +quit > "${rootdir}/tmp/${letter}_raw.txt"
+  # Remove ANSI characters
+  sed -i 's/\x1b//g' "${rootdir}/tmp/${letter}_raw.txt"
+  sed -i 's/\[0m//g' "${rootdir}/tmp/${letter}_raw.txt"
+  sed -i 's/\[1m//g' "${rootdir}/tmp/${letter}_raw.txt"
+  # Remove CWorkThreadPool errors
+  sed -i '/CWorkThreadPool/d' "${rootdir}/tmp/${letter}_raw.txt"
+  sed -i '/workthreadpool.cpp/d' "${rootdir}/tmp/${letter}_raw.txt"
+  sed -i '/CProcessWorkItem/d' "${rootdir}/tmp/${letter}_raw.txt"
+  sed -i '/CHTTPClientThreadPool/d' "${rootdir}/tmp/${letter}_raw.txt"
+  sed -i '/CJobMgr::m_WorkThreadPool:1/d' "${rootdir}/tmp/${letter}_raw.txt"
 
-    # Separating commands and convars
-    # Commands List
-    cat "${rootdir}/tmp/${letter}_raw.txt" > "${rootdir}/tmp/${letter}_commands.txt"
-    sed -i '1,/Commands:/d' "${rootdir}/tmp/${letter}_commands.txt"
-    cat "${rootdir}/tmp/${letter}_commands.txt" >> "${rootdir}/tmp/commands_list_raw.txt"
+  # Separating commands and convars
+  # Commands List
+  cat "${rootdir}/tmp/${letter}_raw.txt" > "${rootdir}/tmp/${letter}_commands.txt"
+  sed -i '1,/Commands:/d' "${rootdir}/tmp/${letter}_commands.txt"
+  cat "${rootdir}/tmp/${letter}_commands.txt" >> "${rootdir}/tmp/commands_list_raw.txt"
 
-    # Convars List
-    cat "${rootdir}/tmp/${letter}_raw.txt" > "${rootdir}/tmp/${letter}convars"
-    sed -i '1,/ConVars:/d' "${rootdir}/tmp/${letter}convars"
-    sed -i '/Commands:/Q' "${rootdir}/tmp/${letter}convars"
-    cat "${rootdir}/tmp/${letter}convars" >> "${rootdir}/tmp/convars_list_raw.txt"
+  # Convars List
+  cat "${rootdir}/tmp/${letter}_raw.txt" > "${rootdir}/tmp/${letter}convars"
+  sed -i '1,/ConVars:/d' "${rootdir}/tmp/${letter}convars"
+  sed -i '/Commands:/Q' "${rootdir}/tmp/${letter}convars"
+  cat "${rootdir}/tmp/${letter}convars" >> "${rootdir}/tmp/convars_list_raw.txt"
 done
 
 # Removing any remaining ANSI characters.
@@ -71,25 +53,21 @@ sort "${rootdir}/tmp/commands_list_raw.txt" | awk '{$1=$1};1' | tee "${rootdir}/
 uniq "${rootdir}/tmp/commands_list_sort.txt" | tee "${rootdir}/tmp/commands_list_uniq.txt"
 sort "${rootdir}/tmp/commands_list_uniq.txt" | tee "${rootdir}/tmp/commands_list.txt"
 
-
 #sort -n "${rootdir}/tmp/convars_list_raw.txt" > "${rootdir}/tmp/convars_list_sort.txt"
-sort "${rootdir}/tmp/convars_list_raw.txt"  | awk '{$1=$1};1' | tee "${rootdir}/tmp/convars_list_sort.txt"
+sort "${rootdir}/tmp/convars_list_raw.txt" | awk '{$1=$1};1' | tee "${rootdir}/tmp/convars_list_sort.txt"
 uniq "${rootdir}/tmp/convars_list_sort.txt" | tee "${rootdir}/tmp/convars_list_uniq.txt"
 sort "${rootdir}/tmp/convars_list_uniq.txt" | tee "${rootdir}/tmp/convars_list.txt"
-
-
- 
 
 # Final Output
 rm "${rootdir}/steamcmd_commands.txt"
 touch "${rootdir}/steamcmd_commands.txt"
 echo "Generating output."
 {
-echo "ConVars:"
-cat "${rootdir}/tmp/convars_list.txt"
-echo ""
-echo "Commands:"
-cat  "${rootdir}/tmp/commands_list.txt"
+  echo "ConVars:"
+  cat "${rootdir}/tmp/convars_list.txt"
+  echo ""
+  echo "Commands:"
+  cat "${rootdir}/tmp/commands_list.txt"
 } > "${rootdir}/steamcmd_commands.txt"
 cat "${rootdir}/steamcmd_commands.txt"
 
