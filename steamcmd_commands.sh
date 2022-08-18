8d60cd359fd025bf3c49517c6d9ddef8897f98be
Daniel Gibbs <me@danielgibbs.co.uk>
2022-08-18 17:34:40 +0100

    lint

--- steamcmd_commands.sh
+++ steamcmd_commands.sh
@@ -17,7 +17,7 @@ cd "${rootdir}/steamcmd" || exit
 # Loop though each letter of the alphabet using find command
 for letter in {a..z}; do
   echo "steamcmd +login anonymous +find ${letter} +quit"
-  steamcmd +login anonymous +find ${letter} +quit >"${rootdir}/tmp/${letter}_raw.txt"
+  steamcmd +login anonymous +find ${letter} +quit > "${rootdir}/tmp/${letter}_raw.txt"
   # Remove ANSI characters
   sed -i 's/\x1b//g' "${rootdir}/tmp/${letter}_raw.txt"
   sed -i 's/\[0m//g' "${rootdir}/tmp/${letter}_raw.txt"
@@ -31,15 +31,15 @@ for letter in {a..z}; do
 
   # Separating commands and convars
   # Commands List
-  cat "${rootdir}/tmp/${letter}_raw.txt" >"${rootdir}/tmp/${letter}_commands.txt"
+  cat "${rootdir}/tmp/${letter}_raw.txt" > "${rootdir}/tmp/${letter}_commands.txt"
   sed -i '1,/Commands:/d' "${rootdir}/tmp/${letter}_commands.txt"
-  cat "${rootdir}/tmp/${letter}_commands.txt" >>"${rootdir}/tmp/commands_list_raw.txt"
+  cat "${rootdir}/tmp/${letter}_commands.txt" >> "${rootdir}/tmp/commands_list_raw.txt"
 
   # Convars List
-  cat "${rootdir}/tmp/${letter}_raw.txt" >"${rootdir}/tmp/${letter}convars"
+  cat "${rootdir}/tmp/${letter}_raw.txt" > "${rootdir}/tmp/${letter}convars"
   sed -i '1,/ConVars:/d' "${rootdir}/tmp/${letter}convars"
   sed -i '/Commands:/Q' "${rootdir}/tmp/${letter}convars"
-  cat "${rootdir}/tmp/${letter}convars" >>"${rootdir}/tmp/convars_list_raw.txt"
+  cat "${rootdir}/tmp/${letter}convars" >> "${rootdir}/tmp/convars_list_raw.txt"
 done
 
 # Removing any remaining ANSI characters.
@@ -68,7 +68,7 @@ echo "Generating output."
   echo ""
   echo "Commands:"
   cat "${rootdir}/tmp/commands_list.txt"
-} >"${rootdir}/steamcmd_commands.txt"
+} > "${rootdir}/steamcmd_commands.txt"
 cat "${rootdir}/steamcmd_commands.txt"
 
 echo "Tidy up."
